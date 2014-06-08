de28d2624103a7c6270cffd846b788ff7d772bef
AnilM³ <anil.mmm@gmail.com>
2014-06-09 00:22:18 +0100

    Added license to unit tests

--- test/main_test_failed.c
+++ test/main_test_failed.c
@@ -1,3 +1,12 @@
+/*******************************************************************************
+* Copyright (C) 2014 Anil Motilal Mahtani Mirchandani(anil.mmm@gmail.com)      *
+*                                                                              *
+* License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>*
+* This is free software: you are free to change and redistribute it.           *
+* There is NO WARRANTY, to the extent permitted by law.                        *
+*                                                                              *
+*******************************************************************************/
+
 #include <arc/test/unit.h>
 
 ARC_TEST(fail)
