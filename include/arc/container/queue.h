aa2f38aadf5aa4af2e4f52256c27e17f92f7aecc
AnilM³ <anil.mmm@gmail.com>
2014-06-26 21:57:54 +0100

    Amended some comments

--- include/arc/container/queue.h
+++ include/arc/container/queue.h
@@ -1,11 +1,11 @@
-/********************************************************************************
-* Copyright (C) 2014 Anil Motilal Mahtani Mirchandani(anil.mmm@gmail.com)       *
-*                                                                               *
-* License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html> *
-* This is free software: you are free to change and redistribute it.            *
-* There is NO WARRANTY, to the extent permitted by law.                         *
-*                                                                               *
-*********************************************************************************/
+/*******************************************************************************
+* Copyright (C) 2014 Anil Motilal Mahtani Mirchandani(anil.mmm@gmail.com)      *
+*                                                                              *
+* License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>*
+* This is free software: you are free to change and redistribute it.           *
+* There is NO WARRANTY, to the extent permitted by law.                        *
+*                                                                              *
+*******************************************************************************/
 /**
  * @file queue.h
  * @author Anil M. Mahtani Mirchandani
--- tools/ArcPerf.sh
+++ tools/ArcPerf.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+################################################################################
+# Copyright (C) 2014 Anil Motilal Mahtani Mirchandani(anil.mmm@gmail.com)      #
+#                                                                              #
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>#
+# This is free software: you are free to change and redistribute it.           #
+# There is NO WARRANTY, to the extent permitted by law.                        #
+#                                                                              #
+################################################################################
 
 if (( $# < 3 ));
 then
