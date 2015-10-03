45b32ecbaec98569351ca9f2dd9674652c78475c
AnilM³ <anil.mmm@gmail.com>
2015-10-04 00:32:16 +0100

    Improved performance test for hash table including random insertion and retrieval

--- perf_test/container/htable_perf.c
+++ perf_test/container/htable_perf.c
@@ -16,6 +16,7 @@
 
 arc_htable_t htable;
 int num_elems = 20000;
+int *values;
 
 arc_hkey_t hash_function(const void *key, size_t size)
 {
@@ -25,17 +26,25 @@ arc_hkey_t hash_function(const void *key, size_t size)
 
 ARC_PERF_FUNCTION(global_set_up)
 {
+    int i;
     const char * num_elems_str = arc_get_param("-n");
 
     if (num_elems_str != NULL)
     {
         num_elems = atoi(num_elems_str);
     }
+
+    values = malloc(sizeof(int) * ((size_t)num_elems));
+
+    for (i = 0; i < num_elems; i++)
+    {
+        values[i] = i;
+    }
 }
 
 ARC_PERF_FUNCTION(global_tear_down)
 {
-
+    free(values);
 }
 
 ARC_PERF_FUNCTION(set_up)
@@ -52,17 +61,22 @@ ARC_PERF_TEST(insert)
     }
 }
 
+ARC_PERF_TEST(random_insert)
+{
+    int i;
+
+    srand(0);
+
+    for (i = 0; i < num_elems; i++)
+    {
+        arc_htable_insert(htable, &values[rand() % num_elems], &i);
+    }
+}
+
+
 ARC_PERF_TEST(rehash)
 {
-/*
-    printf("%lf\n", arc_htable_variance(htable));
-    printf("%u\n", arc_htable_max(htable));
-*/
     arc_htable_rehash(htable, (size_t)num_elems);
-/*
-    printf("%lf\n", arc_htable_variance(htable));
-    printf("%u\n", arc_htable_max(htable));
-*/
 }
 
 ARC_PERF_TEST(retrieve)
@@ -73,6 +87,19 @@ ARC_PERF_TEST(retrieve)
         arc_htable_retrieve(htable, &i);
     }
 }
+
+ARC_PERF_TEST(random_retrieve)
+{
+    int i;
+
+    srand(0);
+
+    for (i = 0; i < num_elems; i++)
+    {
+        arc_htable_retrieve(htable, &values[rand() % num_elems]);
+    }
+}
+
 
 ARC_PERF_FUNCTION(tear_down)
 {
@@ -92,6 +119,17 @@ ARC_PERF_TEST_FIXTURE()
     ARC_PERF_ADD_TEST(insert)
     ARC_PERF_ADD_TEST(rehash)
     ARC_PERF_ADD_TEST(retrieve)
+    ARC_PERF_ADD_FUNCTION(tear_down)
+
+    ARC_PERF_ADD_FUNCTION(set_up)
+    ARC_PERF_ADD_TEST(random_insert)
+    ARC_PERF_ADD_TEST(random_retrieve)
+    ARC_PERF_ADD_FUNCTION(tear_down)
+
+    ARC_PERF_ADD_FUNCTION(set_up)
+    ARC_PERF_ADD_TEST(random_insert)
+    ARC_PERF_ADD_TEST(rehash)
+    ARC_PERF_ADD_TEST(random_retrieve)
     ARC_PERF_ADD_FUNCTION(tear_down)
 
     ARC_PERF_ADD_FUNCTION(global_tear_down)
